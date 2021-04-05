d8d7aba31f56e9509ba0042730d9142123def939
Eduardo Maldonado <d.lynx@MacBook-de-Eduardo.local>
2021-04-04 22:01:09 -0500

    PantallaLogin

---
+++ SmartGarden/SmartGarden/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SmartGarden
+//
+//  Created by Eduardo Maldonado on 04/04/21.
+//  Copyright © 2021 E-Nexus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+    @IBOutlet weak var btn_login_sg: UIButton!
+    @IBOutlet weak var txf_pass_sg: UITextField!
+    @IBOutlet weak var bkg_round: UIView!
+    @IBOutlet weak var txf_mail_sg: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
