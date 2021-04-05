44457f77b576749a2e7b96d7eb52807c38e73fca
Eduardo Maldonado <d.lynx@MacBook-de-Eduardo.local>
2021-04-05 18:32:41 -0500

    Reglas y NavigationController

---
+++ SmartGarden/SmartGarden/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  SmartGarden
+//
+//  Created by Eduardo Maldonado on 05/04/21.
+//  Copyright © 2021 E-Nexus. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Login"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
