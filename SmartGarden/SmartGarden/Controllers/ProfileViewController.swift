0786e1677f525c1862f5a3dc91ccadceaca0cb96
EdwardLynx24 <eduardodarkar24@gmail.com>
2021-04-07 19:06:06 -0500

    Controladores adicionales

---
+++ SmartGarden/SmartGarden/Controllers/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  SmartGarden
+//
+//  Created by Eduardo Maldonado on 07/04/21.
+//  Copyright © 2021 E-Nexus. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var btn_acerca: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
